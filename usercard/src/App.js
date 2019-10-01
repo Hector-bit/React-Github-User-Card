4fe4de6d4961a4c7d1c3d001f1105a3f3c70fca8
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-01 14:45:26 -0700

    added card components

---
+++ usercard/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// <!-- Do NOT change anything here except TODO line -->
+// <html>
+//   <head>
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+//     <link rel="stylesheet" href="./index.css">
+//   </head>
+//   <body>
+//     <div class="container">
+//       <div class="header">
+//         <img src="./assets/lambdalogo.png" alt="Lambda Logo"/>
+//         <p>❤️'s</p>
+//         <img src="./assets/githublogo.png" alt="GitHub Logo" />
+//       </div>
+//       <div class="cards"></div>
+//     </div>
+//     <!-- TODO: add CDN library here -->
+//     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+//     <script src="./GitHubCard/index.js"></script>
+//   </body>
+// </html>
+// <!-- Do NOT change anything here except TODO line -->
