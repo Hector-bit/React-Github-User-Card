98af96d737b4a479ebd107a3e371adc739ccac70
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-02 01:05:45 -0700

    added components to render one card

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,50 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Card from './card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const followersArray = [  
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+  'Hector-bit'
+];
+
+class App extends React.Component {
+  state = {
+    person: [],
+    personText: ''
+  };
+
+  componentDidMount() {
+    // useEffect
+    followersArray.forEach(e => {
+      axios
+      .get(`https://api.github.com/users/${e}`)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          person: res.data
+        });
+      })
+      .catch(err => console.log(err));
+    })
+    }
+
+  render() {
+    console.log(this.state.person);
+    return (
+      <div className='card'>
+      <img src={this.state.person.avatar_url}/>
+      <div className='card-info'>
+          <h3 className='name'>Name: {this.state.person.name}</h3>
+          <p className='username'>Username: {this.state.person.login}</p>
+          <p>Location: {this.state.person.location}</p>
+          <p>
+              <a>{this.state.person.url}</a>
+          </p>
+          <p>Followers: {this.state.person.followers}</p>
+          <p>Following: {this.state.person.following}</p>
+          <p>Bio: {this.state.person.bio}</p>
+      </div>
+  </div>
+    )}
 }
 
 export default App;
-
-
-// <!-- Do NOT change anything here except TODO line -->
-// <html>
-//   <head>
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-//     <link rel="stylesheet" href="./index.css">
-//   </head>
-//   <body>
-//     <div class="container">
-//       <div class="header">
-//         <img src="./assets/lambdalogo.png" alt="Lambda Logo"/>
-//         <p>❤️'s</p>
-//         <img src="./assets/githublogo.png" alt="GitHub Logo" />
-//       </div>
-//       <div class="cards"></div>
-//     </div>
-//     <!-- TODO: add CDN library here -->
-//     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-//     <script src="./GitHubCard/index.js"></script>
-//   </body>
-// </html>
-// <!-- Do NOT change anything here except TODO line -->
